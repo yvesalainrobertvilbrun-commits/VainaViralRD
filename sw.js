a1b89c87aead77d2fcbd4b2ce0cbe6f58fbb4630
Yves Alain Robert Vilbrun <yvesalainrobertvilbrun@gmail.com>
2026-01-08 17:32:38 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,2 @@
-self.addEventListener('install', e => {
-  console.log('Service Worker instalado');
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('install', e => { console.log('Service Worker instalado'); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
